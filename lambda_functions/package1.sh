0022575803561842ba24daecd40e962242a776a2
chenbm4 <chenbm4@gmail.com>
2023-03-30 17:33:04 -0400

    removed unnecessary files and fixed an error

--- lambda_functions/package1.sh
+++
@@ -1,8 +0,0 @@
-
-pip install --target ./package requests-aws4auth opensearch-py inflection
-
-cd package
-zip -r ../deployment1.zip .
-
-cd ..
-zip deployment1.zip LF1.py
--- lambda_functions/package2.sh
+++
@@ -1,8 +0,0 @@
-
-pip install --target ./package requests-aws4auth opensearch-py inflection
-
-cd package
-zip -r ../deployment2.zip .
-
-cd ..
-zip deployment2.zip LF2.py
